268e3783b36312846de21fd0d70a4fdc7221d4d1
ngthecoder <ngoto0208@gmail.com>
2025-07-07 18:07:43 -0700

    Defined ChatroomRequest struct & Create TCP connection

--- client.go
+++ client.go
@@ -7,19 +7,59 @@ import (
 	"os"
 )
 
+type ChatroomHeader struct {
+	RoomNameSize         byte
+	Operation            byte
+	State                byte
+	OperationPayloadSize [29]byte
+}
+
+type ChatroomRequest struct {
+	Header   ChatroomHeader
+	RoomName string
+	Payload  []byte
+}
+
 func main() {
-	serverAddress := &net.UDPAddr{
+	// TCP server for chatroom
+	tcpServerAddr := &net.TCPAddr{
 		IP:   net.ParseIP("127.0.0.1"),
 		Port: 8080,
 		Zone: "",
 	}
-	conn, err := net.DialUDP("udp", nil, serverAddress)
+	tcpConn, err := net.DialTCP("tcp", nil, tcpServerAddr)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	}
-	defer conn.Close()
+	defer tcpConn.Close()
 
+	fmt.Printf("Do you wish to create a new chatroom (Y/N): ")
 	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	newChatroomRequest := scanner.Text()
+	var chatroomName string
+	if newChatroomRequest == "Y" || newChatroomRequest == "y" {
+		fmt.Printf("Enter a name for new chatroom: ")
+		scanner.Scan()
+		chatroomName = scanner.Text()
+	} else {
+		fmt.Printf("Enter which chatroom you wish to join: ")
+		scanner.Scan()
+		chatroomName = scanner.Text()
+	}
+
+	// UDP server for messaging
+	udpServerAddr := &net.UDPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 8090,
+		Zone: "",
+	}
+	udpConn, err := net.DialUDP("udp", nil, udpServerAddr)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	defer udpConn.Close()
+
 	fmt.Printf("Enter username: ")
 	scanner.Scan()
 	username := scanner.Text()
@@ -34,10 +74,10 @@ func main() {
 		load = append(load, []byte(username)...)
 		load = append(load, []byte(message)...)
 
-		conn.Write(load)
+		udpConn.Write(load)
 
 		buff := make([]byte, 1024)
-		n, err := conn.Read(buff)
+		n, err := udpConn.Read(buff)
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 		}
